7c6cf70492055cdf9ebab1b845e43ae3db8f9172
kurahk6 <kurahk6@gmail.com>
2017-04-03 08:14:00 -0700

    Add files via upload

---
+++ WarehouseSuperStore.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-04-03T07:06:26
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = WarehouseSuperStore
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QStringList>
+#include <QDialog>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->addLog, SIGNAL(clicked()), this, SLOT(openExplorer()));
+    connect(ui->saveLog, SIGNAL(clicked()), this, SLOT(saveFile()));
+    connect(ui->newLog, SIGNAL(clicked()), this, SLOT(clearTable()));
+}
+
+void MainWindow::openExplorer()
+{
+    QStringList fileNames = QFileDialog::getOpenFileNames(this, tr("Open File"),"/path/to/file/",tr("Text Files (*.txt)"));
+}
+
+void MainWindow::saveFile()
+{
+    QString fileNames = QFileDialog::getSaveFileName(this, tr("Save File"),"/path/to/file/",tr("Text Files (*.txt)"));
+}
+
+void MainWindow::clearTable()
+{
+    ui->logTable->clear();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
